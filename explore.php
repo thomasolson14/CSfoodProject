f95fe393aa1e0d51bdb12aba037d4c2bc6e6e52d
thomasolson14 <thomasolson14@gmail.com>
2016-03-18 12:02:03 -0400

    Added additional framework
    need to add links and add php to query the database for the new list

--- explore.php
+++ explore.php
@@ -1,15 +1,30 @@
 <html>
   <head>Explore Food</head>
-  
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <body>
   <div class="nav_bar">/*Nav Bar*/</div>
   <div class="center_pane">
-    <div class="block"><div>
-    <div class="block"><div>
-    <div class="block"><div>
-    <div class="more"><div>
-    <div class="more"><div>
-    <div class="more"><div>
+    <div class="block">
+      <img src ="">
+      <h1>Resturant Name</h1>
+    </div>
+    <div class="block">
+      <img src ="">
+      <h1>Resturant Name</h1>
+    </div>
+    <div class="block">
+      <img src ="">
+      <h1>Resturant Name</h1>
+    </div>
+    <div class="more"> 
+      <a href="">Explore cheap food near you</a>
+    </div>
+    <div class="more"> 
+      <a href="">Explore quick food near you</a>
+    </div>
+    <div class="more"> 
+      <a href="">Explore quality food near you</a>
+    </div>
   </div>
   <div class="footer">/*Footer*/</div>
   
