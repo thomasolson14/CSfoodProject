df2f9ad9a23b56521f585edcab553c5521be02de
thomasolson14 <thomasolson14@gmail.com>
2016-03-23 07:32:00 -0400

    Update explore.php

--- explore.php
+++ explore.php
@@ -29,6 +29,33 @@
       <a href="">Explore quality food near you</a>
     </div>
   </div>
+  <div class="divider">
+  </div>
+  <div class="center_pane">
+    <div class = "title">
+      <h1>Cheap</h1>
+    </div>
+    <div class = "list"></div>
+    <div class = "map"></div>
+  </div>
+  <div class="divider">
+  </div>
+  <div class="center_pane">
+    <div class = "title">
+      <h1>Quick</h1>
+    </div>
+    <div class = "list"></div>
+    <div class = "map"></div>
+  </div>
+  <div class="divider">
+  </div>
+  <div class="center_pane">
+    <div class = "title">
+      <h1>Quick</h1>
+    </div>
+    <div class = "list"></div>
+    <div class = "map"></div>
+  </div>
   <div class="footer">/*Footer*/</div>
   
   </body>
